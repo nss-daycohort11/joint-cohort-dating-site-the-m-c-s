ffdcf0283ef72fb358c6c18bb840c55edf020ee4
Mike Ulvila <mikeulvila@me.com>
2015-11-17 16:05:47 -0600

    profile view handlebar template modified

--- javascripts/home_view.js
+++ javascripts/home_view.js
@@ -3,6 +3,7 @@ define(["jquery", "user_data_promise", "hbs"], function($, userData, Handlebars)
 		.then(function(data) {
 			console.log("userData", data);
 			var randomCandidate = _.sample(data.users, 3);
+			console.log("randomCandidate", randomCandidate);
 			require(['hbs!../templates/candidates'], function (candidateTemplate) {
         $("#mini-profile-view-row1").append(candidateTemplate({users: randomCandidate})); 	
       });
